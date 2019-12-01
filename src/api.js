6d3c9cd12fc52cf85b9056fd332f952585fe2d09
natyeo <49326857+natyeo@users.noreply.github.com>
2019-12-01 20:47:17 +0000

    api function created

---
+++ src/api.js
@@ -0,0 +1,26 @@
+(function(exports) {
+  function GuardianAPI() {
+    this.data = [];
+  }
+
+  GuardianAPI.prototype.getTodayData = function() {
+    var today = new Date().toISOString().slice(0,10);
+    const url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=https://content.guardianapis.com/search?section=uk-news&from-date=${today}&order-by=newest&show-fields=headline';
+
+    this.newRequest(url);
+  }
+
+  GuardianAPI.prototype.newRequest = function(url) {
+    let self = this;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        self.data = request.response.results;
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  }
+
+  exports.GuardianAPI = GuardianAPI
+})(this);
